77a7582781874560222f68b2a4384f01af439ee2
JoshyPHP <s9e.dev@gmail.com>
2018-07-11 22:04:05 +0200

    Added support for explicit language declaration

--- s9e/highlighter/listener.php
+++ s9e/highlighter/listener.php
@@ -19,14 +19,34 @@ class listener implements EventSubscriberInterface
 	public function onConfigure($event)
 	{
 		$configurator = $event['configurator'];
-		if (!isset($configurator->tags['CODE']))
+		if (!isset($configurator->tags['CODE'], $configurator->BBCodes['CODE']))
 		{
 			return;
 		}
 
+		$configurator->BBCodes['CODE']->defaultAttribute = 'lang';
+		if (!isset($configurator->tags['CODE']->attributes['lang']))
+		{
+			$attribute = $configurator->tags['CODE']->attributes->add('lang');
+			$attribute->required = false;
+			$attribute->filterChain->append('#identifier');
+		}
+
+		$attribute = $configurator->tags['CODE']->attributes['lang'];
+		$attribute->filterChain->prepend('#map')->setMap(
+			[
+				'c#'  => 'csharp',
+				'c++' => 'cpp',
+				'f#'  => 'fsharp'
+			],
+			false
+		);
+		$attribute->filterChain->prepend('strtolower');
+
 		$dom = $configurator->tags['CODE']->template->asDOM();
 		foreach ($dom->getElementsByTagName('code') as $code)
 		{
+			$code->setAttribute('class', trim($code->getAttribute('class') . ' {@lang}'));
 			$code->setAttribute('data-lang', '{@lang}');
 		}
 		$dom->saveChanges();
